35fa09093fa111d1e55f27276d4cf5bc139db462
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2023-10-29 12:36:34 +0530

    Add todo

--- tests/test_files/local_variables.py
+++ tests/test_files/local_variables.py
@@ -18,6 +18,15 @@ def dunderify(tree: ast.AST) -> None:
 
 print(eval(ast.unparse(ast.parse("1 + 2"))))
 
+# TODO: right now user defined functions, classes are not registered as a Store.
+# So function names and invocations are not renamed.
+# Also, if you do:
+# def any(elems):
+#    ...
+# print(any([True, False]))
+# it will use the builtin any instead of the function any.
+
+
 # TODO: add test for global and nonlocal
 # TODO: add test for overriding builtins
 # TODO: add test for overriding builtins in a parent scope
